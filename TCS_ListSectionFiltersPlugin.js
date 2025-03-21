00c2be32bc682adc817a4ee17302c3f1d8e5ce48
Alistair Everington <hello@thecodeshop.co>
2025-03-21 10:12:07 +0000

    Add files via upload

--- TCS_ListSectionFiltersPlugin.js
+++ TCS_ListSectionFiltersPlugin.js
@@ -282,7 +282,7 @@ function initialiseListSectionFilters() {
                 });
     
                 // Resolve the promise after the filtering animations are complete
-                setTimeout(resolve, 255); // Ensure all animations are complete
+                setTimeout(resolve, 520); // Ensure all animations are complete
             });
         };
     
